a59ac7dbf98a14f6a422c0e6c21916e01907632f
Wes Hendley <wrhendley@gmail.com>
2023-06-14 16:39:28 -0500

    Fix #insert

--- lib/tree.rb
+++ lib/tree.rb
@@ -31,7 +31,7 @@ class Tree
     return if value == @root.data || value.nil?
 
     tmp = @root
-    tmp = value < tmp.data ? tmp.left : tmp.right until tmp.nil?
+    tmp = value < tmp.data ? tmp.left : tmp.right until tmp.left.nil? || tmp.right.nil?
 
     value < tmp.data ? tmp.left = Node.new(value) : tmp.right = Node.new(value)
   end
