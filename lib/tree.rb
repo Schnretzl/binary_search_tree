d9c93bd44fec230b97974325719c6f5be00d5ac7
Wes Hendley <wrhendley@gmail.com>
2023-06-14 16:33:24 -0500

    Fix to make Tree composed of all Nodes, instead of Trees.  Add #insert (broken)

--- lib/tree.rb
+++ lib/tree.rb
@@ -13,17 +13,26 @@ class Tree
     return nil if first_index > last_index || arr.nil?
 
     mid = (first_index + last_index) / 2
-    @root = Node.new(arr[mid])
-    @root.left = Tree.new(arr, first_index, mid - 1)
-    @root.right = Tree.new(arr, mid + 1, last_index)
-    @root
+    root = Node.new(arr[mid])
+    root.left = build_tree(arr, first_index, mid - 1)
+    root.right = build_tree(arr, mid + 1, last_index)
+    root
   end
 
   def pretty_print(node = @root, prefix = '', is_left = true)
     return if node.nil?
 
-    pretty_print(node.right.root, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
+    pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
     puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
-    pretty_print(node.left.root, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
+    pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
+  end
+
+  def insert(value)
+    return if value == @root.data || value.nil?
+
+    tmp = @root
+    tmp = value < tmp.data ? tmp.left : tmp.right until tmp.nil?
+
+    value < tmp.data ? tmp.left = Node.new(value) : tmp.right = Node.new(value)
   end
 end
